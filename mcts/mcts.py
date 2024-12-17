b690a87abc1d2bb00ddbb0e71dc9c3f5e72e8044
liuhanzuo <lhz24@mails.tsinghua.edu.cn>
2024-12-17 13:26:47 +0800

    test data

--- mcts/mcts.py
+++ mcts/mcts.py
@@ -3,9 +3,10 @@ import numpy as np
 from multiprocessing import Pool
 import itertools
 from tqdm import tqdm
+import pandas as pd
 import matplotlib.pyplot as plt
 import pkg.gaming as gaming
-
+import os
 def main(selection1 = None, selection2 = None):
     """ Play a several game to the end using UCT for both players.
     """
@@ -15,12 +16,11 @@ def main(selection1 = None, selection2 = None):
 
     results_list = []
     # processing
-    pool = Pool(processes=32)
     win_rate1 = []
     win_rate2 = []
     draw_rate = []
     for j in range(1, 20):
-        args=([(number_of_games, number_of_games, False, 10 * j, 40, selection1, selection2)])
+        args=([(number_of_games, number_of_games, False, 10 * j, 80, selection1, selection2)])
         win1, win2, draw = gaming.PlayGame(args[0])
         print("Player1 wins: %2.2f%%" % (win1/number_of_games*100))
         print("Player2 wins: %2.2f%%" % (win2/number_of_games*100))
@@ -37,12 +37,12 @@ def main(selection1 = None, selection2 = None):
     plt.ylabel('Win Rate (%)')
     plt.title(f'{selection1} vs {selection2}')
     plt.legend()
-    plt.savefig(f'figs/40/{selection1} vs {selection2}1.png')
+    plt.savefig(f'figs/80/{selection1} vs {selection2}1.png')
     win_rate1 = []
     win_rate2 = []
     draw_rate = []
     for j in range(1, 20):
-        args=([(number_of_games, number_of_games, False, 40, 10*j, selection2, selection1)])
+        args=([(number_of_games, number_of_games, False, 80, 10*j, selection2, selection1)])
         win1, win2, draw = gaming.PlayGame(args[0])
         print("Player1 wins: %2.2f%%" % (win1/number_of_games*100))
         print("Player2 wins: %2.2f%%" % (win2/number_of_games*100))
@@ -58,12 +58,12 @@ def main(selection1 = None, selection2 = None):
     plt.ylabel('Win Rate (%)')
     plt.title(f'{selection2} vs {selection1}')
     plt.legend()
-    plt.savefig(f'figs/40/{selection2} vs {selection1}1.png')
+    plt.savefig(f'figs/80/{selection2} vs {selection1}1.png')
     win_rate1 = []
     win_rate2 = []
     draw_rate = []
     for j in range(1, 20):
-        args=([(number_of_games, number_of_games, False, 10 * j, 40, selection1, selection2)])
+        args=([(number_of_games, number_of_games, False, 10 * j, 80, selection1, selection2)])
         win1, win2, draw = gaming.PlayRandomGame(args[0])
         print("Player1 wins: %2.2f%%" % (win1/number_of_games*100))
         print("Player2 wins: %2.2f%%" % (win2/number_of_games*100))
@@ -71,6 +71,7 @@ def main(selection1 = None, selection2 = None):
         win_rate1.append(win1/number_of_games*100)
         win_rate2.append(win2/number_of_games*100)
         draw_rate.append(draw/number_of_games*100)
+        print(win1, win2, draw)
     plt.clf()
     plt.plot(range(1, 20), win_rate1, label=f'{selection1} first Win Rate')
     plt.plot(range(1, 20), win_rate2, label=f'{selection2} second Win Rate')
@@ -79,9 +80,50 @@ def main(selection1 = None, selection2 = None):
     plt.ylabel('Win Rate (%)')
     plt.title(f'{selection1} vs {selection2}')
     plt.legend()
-    plt.savefig(f'figs/40/{selection1} vs {selection2}2.png')
+    plt.savefig(f'figs/80/{selection1} vs {selection2}2.png')
+def playagainst(selection1 = None, selection2 = None, filename = None, epochs = 0):
+    """ Play a several game to the end using UCT for both players.
+    """
+
+    number_of_games = 10000
+    args=([(number_of_games, number_of_games, False, epochs, epochs, selection1, selection2)])
+    win1, win2, draw = gaming.PlayRandomGame(args[0])
+    with open (filename, 'a') as f:
+        f.write(f"{selection1} vs {selection2}, {selection1} wins: %2.2f%%\n" % (win1/number_of_games*100))
+        f.write(f"{selection1} vs {selection2}, {selection2} wins: %2.2f%%\n" % (win2/number_of_games*100))
+        f.write(f"{selection1} vs {selection2}, Draws: %2.2f%%\n" % (draw/number_of_games*100))
+        f.write("\n")
+    print("Player1 wins: %2.2f%%" % (win1/number_of_games*100))
+    print("Player2 wins: %2.2f%%" % (win2/number_of_games*100))
+    print("Draws: %2.2f%%" % (draw/number_of_games*100))
+
 if __name__ == "__main__":
+    # selections = ["reward"]
     selections = ['UCT', 'UCB1tuned', 'reward', 'visited']
-    for i in range(len(selections)):
-        for j in range(i, len(selections)):
-            main(selections[i], selections[j])
+    epoch_list = [20, 40, 60, 80, 100, 120]
+    for epochs in epoch_list:
+        print(f'Epochs: {epochs}')
+        filename = f'figs/csv/results_{epochs}.csv'
+        if not os.path.exists(os.path.dirname(filename)):
+            os.makedirs(os.path.dirname(filename))
+        # playagainst(selections[i], selections[j], filename, epochs)
+        # Initialize a DataFrame to store the results
+        results_df = pd.DataFrame(columns=['Selection1', 'Selection2', 'WinRate1', 'WinRate2', 'DrawRate'])
+        # Populate the DataFrame with the results
+        for i in range(len(selections)):
+            for j in range(i, len(selections)):
+                number_of_games = 10000
+                args = [(number_of_games, number_of_games, False, epochs, epochs, selections[i], selections[j])]
+                win1, win2, draw = gaming.PlayRandomGame(args[0])
+                new_row = pd.DataFrame([{
+                    'Selection1': selections[i],
+                    'Selection2': selections[j],
+                    'WinRate1': win1 / number_of_games * 100,
+                    'WinRate2': win2 / number_of_games * 100,
+                    'DrawRate': draw / number_of_games * 100
+                }])
+                results_df = pd.concat([results_df, new_row], ignore_index=True)
+        # Print the DataFrame
+        print(results_df)
+        # Save the DataFrame to a CSV file
+        results_df.to_csv(filename, index=False)
--- mcts/pkg/gaming.py
+++ mcts/pkg/gaming.py
@@ -2,44 +2,26 @@ import pkg.tictactoe as tictactoe
 import random
 import pkg.agents as agents
 from tqdm import tqdm
-def PlayGame(args):
-    train_game_number, test_game_number, verbose, itermax1, itermax2, selection1, selection2 = args
-    """ Play a sample game between two UCT players where each player gets a different number
-        of UCT iterations (= simulations = tree nodes).
-    """
-    assert selection1 is not None
-    assert selection2 is not None
+from multiprocessing import Pool
+def PlaySingleGame(args):
+    verbose, agent1, agent2, random_flag, test = args
     state = tictactoe.TicTacToe()
-    agent1 = agents.Agent(itermax=itermax1, verbose=False, selection=selection1)
-    agent2 = agents.Agent(itermax=itermax2, verbose=False, selection=selection2)
-    for _ in tqdm(range(train_game_number), desc="Playing games"):
-        while (state.GetMoves() != []):
-            if verbose:
-                print(str(state))
-            if state.LastPlayer() == 1:
-                m = agent1.predict(state)
-            else:
-                m = agent2.predict(state)
-            if verbose: print("Best Move: " + str(m) + "\n")
-            state.DoMove(m)
-
-            if state.HasWinning(): break
+    if random_flag:
+        state.SetLastPlayer(random.choice([1, 2]))
     win1 = 0
-    win2 = 0 
+    win2 = 0
     draw = 0
-    for _ in tqdm(range(test_game_number), desc="Playing games"):
-        while (state.GetMoves() != []):
-            if verbose:
-                print(str(state))
-            if state.LastPlayer() == 1:
-                m = agent1.predict(state)
-            else:
-                m = agent2.predict(state)
-            if verbose: print("Best Move: " + str(m) + "\n")
-            state.DoMove(m)
-
-            if state.HasWinning(): break
-
+    while (state.GetMoves() != []):
+        if verbose:
+            print(str(state))
+        if state.LastPlayer() == 1:
+            m = agent1.predict(state)
+        else:
+            m = agent2.predict(state)
+        if verbose: print("Best Move: " + str(m) + "\n")
+        state.DoMove(m)
+        if state.HasWinning(): break
+    if test:
         result = state.GetResult(state.LastPlayer())
         winning = False
         winner = None
@@ -65,6 +47,29 @@ def PlayGame(args):
         else:
             win2 += 1
     return (win1, win2, draw)
+def PlayGame(args):
+    train_game_number, test_game_number, verbose, itermax1, itermax2, selection1, selection2 = args
+    """ Play a sample game between two UCT players where each player gets a different number
+        of UCT iterations (= simulations = tree nodes).
+    """
+    assert selection1 is not None
+    assert selection2 is not None
+    agent1 = agents.Agent(itermax=itermax1, verbose=False, selection=selection1)
+    agent2 = agents.Agent(itermax=itermax2, verbose=False, selection=selection2)
+    pool = Pool(processes=32)
+    for result in tqdm(pool.imap_unordered(PlaySingleGame, [(verbose, agent1, agent2, True, False)] * train_game_number),
+         total=train_game_number):
+        pass
+        
+    win1 = 0
+    win2 = 0 
+    draw = 0
+    for result in tqdm(pool.imap_unordered(PlaySingleGame, [(verbose, agent1, agent2, False, True)] * test_game_number),
+            total=test_game_number):
+        win1 += result[0]
+        win2 += result[1]
+        draw += result[2]
+    return (win1, win2, draw)
 
 def PlayRandomGame(args):
     train_game_number, test_game_number, verbose, itermax1, itermax2, selection1, selection2 = args
@@ -73,60 +78,19 @@ def PlayRandomGame(args):
     """
     assert selection1 is not None
     assert selection2 is not None
-    state = tictactoe.TicTacToe()
-    state.SetLastPlayer(random.choice([1, 2]))
     agent1 = agents.Agent(itermax=itermax1, verbose=False, selection=selection1)
     agent2 = agents.Agent(itermax=itermax2, verbose=False, selection=selection2)
-    for _ in tqdm(range(train_game_number), desc="Playing games"):
-        while (state.GetMoves() != []):
-            if verbose:
-                print(str(state))
-            if state.LastPlayer() == 1:
-                m = agent1.predict(state)
-            else:
-                m = agent2.predict(state)
-            if verbose: print("Best Move: " + str(m) + "\n")
-            state.DoMove(m)
-
-            if state.HasWinning(): break
+    pool = Pool(processes=32)
+    for result in tqdm(pool.imap_unordered(PlaySingleGame, [(verbose, agent1, agent2, True, False)] * train_game_number),
+         total=train_game_number):
+        pass
+        
     win1 = 0
     win2 = 0 
     draw = 0
-    for _ in tqdm(range(test_game_number), desc="Playing games"):
-        while (state.GetMoves() != []):
-            if verbose:
-                print(str(state))
-            if state.LastPlayer() == 1:
-                m = agent1.predict(state)
-            else:
-                m = agent2.predict(state)
-            if verbose: print("Best Move: " + str(m) + "\n")
-            state.DoMove(m)
-
-            if state.HasWinning(): break
-
-        result = state.GetResult(state.LastPlayer())
-        winning = False
-        winner = None
-        if result == 1:
-            winner = state.LastPlayer()
-            winning = True
-            if verbose:
-                print("Player " + str(state.LastPlayer()) + " wins!")
-        elif result == -1:
-            winner = 3 - state.LastPlayer()
-            winning = True
-            if verbose:
-                print("Player " + str(3 - state.LastPlayer()) + " wins!")
-        else:
-            if verbose:
-                print("Nobody wins!")
-        if verbose:
-            print(str(state))
-        if not winning:
-            draw += 1
-        elif winner == 1:
-            win1 += 1
-        else:
-            win2 += 1
+    for result in tqdm(pool.imap_unordered(PlaySingleGame, [(verbose, agent1, agent2, True, True)] * test_game_number),
+            total=test_game_number):
+        win1 += result[0]
+        win2 += result[1]
+        draw += result[2]
     return (win1, win2, draw)
